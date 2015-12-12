26039ffc11afa6ae8054072d18aed64123eb0ad4
rascal <stayrascal@gmail.com>
2015-12-12 23:22:23 +0800

    init project

---
+++ src/main/java/com/tw/bookshelf/SpringBootWebApplication.java
@@ -0,0 +1,23 @@
+package com.tw.bookshelf;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class SpringBootWebApplication {
+
+    public static void main(String[] args){
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootWebApplication.class, args);
+        String[] names = applicationContext.getBeanDefinitionNames();
+
+        Arrays.sort(names);
+        System.out.println("applicationContext via Spring Boot:");
+        for (String name : names){
+            System.out.println(name);
+        }
+    }
+}
