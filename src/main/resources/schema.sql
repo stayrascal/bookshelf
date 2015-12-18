5b79b8aa767cbc4541396420f57915215ca3f7a0
rascal <stayrascal@gmail.com>
2015-12-18 19:48:23 +0800

    remove resource

--- src/main/resources/schema.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO book(isbn, name, author, price) values("9780201485677", "Refactoring", "Martin Fowler", 64.99);
-INSERT INTO book(isbn, name, author, price) values("9780132350884", "Clean Code", "Robert C. Martin", 35.44);
-INSERT INTO book(isbn, name, author, price) values("9780131429017", "The Art of UNIX Programming", "Eric S. Raymond", 39.99);
